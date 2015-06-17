5e1da1647e3af414931c52dd266fc753443d4170
Tiago Rodrigues <tmcrodrigues@gmail.com>
2015-06-17 22:12:16 +0200

    using props

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import MyFirstComponent from './MyFirstComponent';
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello, world.</h1>
-        <MyFirstComponent />
-      </div>
+      <MyFirstComponent message="hello world" />
     );
   }
 }
--- src/MyFirstComponent.js
+++ src/MyFirstComponent.js
@@ -1,22 +1,9 @@
 import React, { Component } from 'react';
 
 export default class MyFirstComponent extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      items: [
-        {label: 'item 1', quantity: 4},
-        {label: 'item 2', quantity: 3}
-      ]
-    };
-  }
-
   render() {
-    var items = this.state.items.map(item => {
-      return <li>Item: {item.label}, Quantity: {item.quantity}</li>;
-    });
     return (
-      <ul>{items}</ul>
+      <h1>{this.props.message}</h1>
     );
   }
 };
